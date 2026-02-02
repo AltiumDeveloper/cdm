a30241a4b37c4f9852e0c8b07eae3a0ec11db2ab
David Hlavaty <82456349+davidhlavaty@users.noreply.github.com>
2026-02-02 14:00:09 +0000

    Update SDM to reflect latest proposal (#10)
    * Update SDM to reflect the current proposal
    
    * Update SDM to use dmConfiguredDeviceModel and write basic test for ESD client

--- tests/clients/e2_client.py
+++ tests/clients/e2_client.py
@@ -1,27 +1,27 @@
-import common_data_model.datamodel.common_data_model as cdm
-from tests.clients.sim_client import SIMClient
-from tests.sim import SIM
-
-
-class E2Client(SIMClient):
-
-    model: cdm.SftSoftwareBom
-    baseline: SIM
-
-    def __init__(self, model: cdm.SftSoftwareBom = None) -> None:
-        self.model = model if model else cdm.SftSoftwareBom(id="sbom-1", components=[])
-        pass
-
-    def push_to_sim(self, sim: SIM):
-        SIMClient.ensure_up_to_date(sim, self.baseline)
-
-        # update SIM - trivial "full update" for now
-        sim.update_software_model(self.model)
-        # save the new baseline version of the SIM
-        self.baseline = sim.snapshot()
-
-    def pull_from_sim(self, sim: SIM):
-        # TODO: perform ECO
-
-        # save the new baseline version of the SIM
-        self.baseline = sim.snapshot()
+# import common_data_model.datamodel.common_data_model as cdm
+# from tests.clients.sim_client import SIMClient
+# from tests.sim import SIM
+#
+#
+# class E2Client(SIMClient):
+#
+#     model: cdm.SftSoftwareBom
+#     baseline: SIM
+#
+#     def __init__(self, model: cdm.SftSoftwareBom = None) -> None:
+#         self.model = model if model else cdm.SftSoftwareBom(id="sbom-1", components=[])
+#         pass
+#
+#     def push_to_sim(self, sim: SIM):
+#         SIMClient.ensure_up_to_date(sim, self.baseline)
+#
+#         # update SIM - trivial "full update" for now
+#         sim.update_software_model(self.model)
+#         # save the new baseline version of the SIM
+#         self.baseline = sim.snapshot()
+#
+#     def pull_from_sim(self, sim: SIM):
+#         # TODO: perform ECO
+#
+#         # save the new baseline version of the SIM
+#         self.baseline = sim.snapshot()
